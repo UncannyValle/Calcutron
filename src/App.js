ecc65c51ecada60a94301d47bc66313d0263de25
uncannyvalle <jvalle0926@gmail.com>
2020-03-06 00:04:19 -0800

    made some changes to logic

--- src/App.js
+++ src/App.js
@@ -1,29 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
 import NumberPad from "./components/NumberPad";
 
 function App() {
+  const [result, setResult] = useState(0);
+
+  const buttonClick = value => {
+    let number = result;
+    if (value === "clear") {
+      number = 0 
+      setResult("0");
+    } else if (number === 0 && value > 0) {
+      number = value
+      setResult(value);
+    } else if (number === 0 && value === ".") {
+      setResult(0 + value);
+    } else if (number > 0) {
+      setResult("" + result + value);
+    }
+    console.log("click");
+  };
+
   return (
     <>
       <header>
         <h1>Animal Calculator</h1>
       </header>
+
       <NumberPad>
-      <Button id={"one"} name={1} />
-      <Button id={"two"} name={2} />
-      <Button id={"three"} name={3} />
-      <Button id={"plus"} name={""} />
+        <input id="display" type="number" readOnly value={result}></input>
+        <Button id={"one"} name={1} clickHandler={() => buttonClick(1)} />
+        <Button id={"two"} name={2} clickHandler={() => buttonClick(2)} />
+        <Button id={"three"} name={3} clickHandler={() => buttonClick(3)} />
+        <Button id={"multiply"} name={"*"} />
 
-      <Button id={"four"} name={4} />
-      <Button id={"five"} name={5} />
-      <Button id={"six"} name={6} />
-      <Button id={"seven"} name={7} />
-      <Button id={"eight"} name={8} />
-      <Button id={"nine"} name={9} />
-      <Button id={"zero"} name={0} />
-      
+        <Button id={"four"} name={4} clickHandler={() => buttonClick(4)} />
+        <Button id={"five"} name={5} clickHandler={() => buttonClick(5)} />
+        <Button id={"six"} name={6} clickHandler={() => buttonClick(6)} />
+        <Button id={"divide"} name={"/"} />
 
+        <Button id={"seven"} name={7} clickHandler={() => buttonClick(7)} />
+        <Button id={"eight"} name={8} clickHandler={() => buttonClick(8)} />
+        <Button id={"nine"} name={9} clickHandler={() => buttonClick(9)} />
+        <Button id={"add"} name={"+"} />
+
+        <Button id={"zero"} name={0} clickHandler={() => buttonClick(0)} />
+        <Button
+          id={"decimal"}
+          name={"."}
+          clickHandler={() => buttonClick(".")}
+        />
+
+        <Button id={"subtract"} name={"-"} />
+        <Button
+          id={"clear"}
+          name={"CE"}
+          clickHandler={() => buttonClick("clear")}
+        />
+
+        <Button id={"equals"} name={"="} />
       </NumberPad>
     </>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-const Button = props => <button className="button" id={props.id}>{props.name}</button>;
+const Button = (props) => (
+  <button className="button" onClick={props.clickHandler} id={props.id} value={props.name}>
+  {props.name}
+  </button>
+);
 
 export default Button;
