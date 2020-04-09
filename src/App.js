6ff651d4e1f7161c6fecd0c85d31680a07627767
uncannyvalle <jvalle0926@gmail.com>
2020-04-09 14:59:34 -0600

    display input works

--- src/App.js
+++ src/App.js
@@ -10,10 +10,14 @@ function App() {
   const [operator, setOperator] = useState("");
 
   const addToDisplay = (val) => {
-    setDisplay(display + val);
+    if (display === "0") {
+      setDisplay("" + val);
+    } else {
+      setDisplay(display + val);
+    }
   };
   const addZeroToDisplay = (val) => {
-    if (display !== "") {
+    if (display !== "0") {
       setDisplay(display + val);
     }
   };
@@ -74,22 +78,54 @@ function App() {
         <Button clickHandler={addToDisplay} id={"one"}>
           1
         </Button>
-        <Button clickHandler={addToDisplay}id={"two"}>2</Button>
-        <Button clickHandler={addToDisplay}id={"three"}>3</Button>
-        <Button clickHandler={addition}id={"add"}>+</Button>
-        <Button clickHandler={addToDisplay}id={"four"}>4</Button>
-        <Button clickHandler={addToDisplay}id={"five"}>5</Button>
-        <Button clickHandler={addToDisplay}id={"six"}>6</Button>
-        <Button clickHandler={subtraction}id={"subtract"}>-</Button>
-        <Button clickHandler={addToDisplay}id={"seven"}>7</Button>
-        <Button clickHandler={addToDisplay}id={"eight"}>8</Button>
-        <Button clickHandler={addToDisplay}id={"nine"}>9</Button>
-        <Button clickHandler={multiplication}id={"multiply"}>*</Button>
-        <Button clickHandler={addDecimalToDisplay}id={"decimal"}>.</Button>
-        <Button clickHandler={addZeroToDisplay}id={"zero"}>0</Button>
-        <Button clickHandler={clearAll}id={"clear"}>CE</Button>
-        <Button clickHandler={division}id={"divide"}>/</Button>
-        <Button clickHandler={answer}id={"equals"}>=</Button>
+        <Button clickHandler={addToDisplay} id={"two"}>
+          2
+        </Button>
+        <Button clickHandler={addToDisplay} id={"three"}>
+          3
+        </Button>
+        <Button clickHandler={addition} id={"add"}>
+          +
+        </Button>
+        <Button clickHandler={addToDisplay} id={"four"}>
+          4
+        </Button>
+        <Button clickHandler={addToDisplay} id={"five"}>
+          5
+        </Button>
+        <Button clickHandler={addToDisplay} id={"six"}>
+          6
+        </Button>
+        <Button clickHandler={subtraction} id={"subtract"}>
+          -
+        </Button>
+        <Button clickHandler={addToDisplay} id={"seven"}>
+          7
+        </Button>
+        <Button clickHandler={addToDisplay} id={"eight"}>
+          8
+        </Button>
+        <Button clickHandler={addToDisplay} id={"nine"}>
+          9
+        </Button>
+        <Button clickHandler={multiplication} id={"multiply"}>
+          *
+        </Button>
+        <Button clickHandler={addDecimalToDisplay} id={"decimal"}>
+          .
+        </Button>
+        <Button clickHandler={addZeroToDisplay} id={"zero"}>
+          0
+        </Button>
+        <Button clickHandler={clearAll} id={"clear"}>
+          CE
+        </Button>
+        <Button clickHandler={division} id={"divide"}>
+          /
+        </Button>
+        <Button clickHandler={answer} id={"equals"}>
+          =
+        </Button>
       </NumberPad>
     </>
   );
